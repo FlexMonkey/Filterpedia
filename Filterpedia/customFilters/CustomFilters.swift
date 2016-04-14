794fd81b5b9e7dd7763410ab06e812dc4feecd3c
Simon Gladman <sgladman@gmail.com>
2016-04-14 08:08:07 +0100

    Added normal map filter

--- Filterpedia/customFilters/CustomFilters.swift
+++ Filterpedia/customFilters/CustomFilters.swift
@@ -271,6 +271,13 @@ class CustomFiltersVendor: NSObject, CIFilterConstructor
                                     classAttributes: [
                                         kCIAttributeFilterCategories: [CategoryCustomFilters]
             ])
+        
+
+        CIFilter.registerFilterName("NormalMap",
+                                    constructor: CustomFiltersVendor(),
+                                    classAttributes: [
+                                        kCIAttributeFilterCategories: [CategoryCustomFilters]
+            ])
 
     }
     
@@ -391,6 +398,9 @@ class CustomFiltersVendor: NSObject, CIFilterConstructor
             
         case "CausticRefraction":
             return CausticRefraction()
+            
+        case "NormalMap":
+            return NormalMapFilter()
             
         case "MetalPixellateFilter":
             #if !arch(i386) && !arch(x86_64)
@@ -1227,4 +1237,80 @@ class VignetteNoirFilter: CIFilter
         return blendWithMaskFilter?.outputImage
     }
 }
+
+// MARK: Normal Map Filter
+
+/// NormalMapFilter - converts a bump map to a normal map
+class NormalMapFilter: CIFilter
+{
+    var inputImage: CIImage?
+    var inputContrast = CGFloat(2)
+    
+    override var attributes: [String : AnyObject]
+    {
+        return [
+            kCIAttributeFilterDisplayName: "Normap Map",
+            
+            "inputImage": [kCIAttributeIdentity: 0,
+                kCIAttributeClass: "CIImage",
+                kCIAttributeDisplayName: "Image",
+                kCIAttributeType: kCIAttributeTypeImage],
+            
+            "inputContrast": [kCIAttributeIdentity: 0,
+                kCIAttributeClass: "NSNumber",
+                kCIAttributeDefault: 2,
+                kCIAttributeDisplayName: "Contrast",
+                kCIAttributeMin: 1,
+                kCIAttributeSliderMin: 1,
+                kCIAttributeSliderMax: 6,
+                kCIAttributeType: kCIAttributeTypeScalar],
+        ]
+    }
+    
+    let normalMapKernel = CIKernel(string:
+        "float lumaAtOffset(sampler source, vec2 origin, vec2 offset)" +
+        "{" +
+        " vec3 pixel = sample(source, samplerTransform(source, origin + offset)).rgb;" +
+        " float luma = dot(pixel, vec3(0.2126, 0.7152, 0.0722));" +
+        " return luma;" +
+        "}" +
+        
+        
+        "kernel vec4 normalMap(sampler image) \n" +
+        "{ " +
+        " vec2 d = destCoord();" +
+        
+        " float northLuma = lumaAtOffset(image, d, vec2(0.0, -1.0));" +
+        " float southLuma = lumaAtOffset(image, d, vec2(0.0, 1.0));" +
+        " float westLuma = lumaAtOffset(image, d, vec2(-1.0, 0.0));" +
+        " float eastLuma = lumaAtOffset(image, d, vec2(1.0, 0.0));" +
+       
+        " float horizontalSlope = ((westLuma - eastLuma) + 1.0) * 0.5;" +
+        " float verticalSlope = ((northLuma - southLuma) + 1.0) * 0.5;" +
+       
+        
+        " return vec4(horizontalSlope, verticalSlope, 1.0, 1.0);" +
+        "}"
+    )
+    
+    override var outputImage: CIImage?
+    {
+        guard let inputImage = inputImage,
+            normalMapKernel = normalMapKernel else
+        {
+            return nil
+        }
+        
+        return normalMapKernel.applyWithExtent(inputImage.extent,
+            roiCallback:
+            {
+                (index, rect) in
+                return rect
+            },
+            arguments: [inputImage])?
+            .imageByApplyingFilter("CIColorControls", withInputParameters: [kCIInputContrastKey: inputContrast])
+    }
+}
+
+
 
