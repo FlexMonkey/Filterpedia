3c78cf9d0e923ff7c76071cd31667b70b1fac51e
Simon Gladman <sgladman@gmail.com>
2016-05-18 10:00:34 +0100

    Alternative scatter filter based on warp kernel for blog post

--- Filterpedia/customFilters/CustomFilters.swift
+++ Filterpedia/customFilters/CustomFilters.swift
@@ -424,6 +424,13 @@ class CustomFiltersVendor: NSObject, CIFilterConstructor
             classAttributes: [
                 kCIAttributeFilterCategories: [CategoryCustomFilters]
             ])
+        
+        CIFilter.registerFilterName(
+            "ScatterWarp",
+            constructor: CustomFiltersVendor(),
+            classAttributes: [
+                kCIAttributeFilterCategories: [CategoryCustomFilters]
+            ])
     }
   
     func filterWithName(name: String) -> CIFilter?
@@ -588,6 +595,9 @@ class CustomFiltersVendor: NSObject, CIFilterConstructor
             
         case "Scatter":
             return Scatter()
+            
+        case "ScatterWarp":
+            return ScatterWarp()
             
         case "MetalPixellateFilter":
             #if !arch(i386) && !arch(x86_64)
--- Filterpedia/customFilters/Scatter.swift
+++ Filterpedia/customFilters/Scatter.swift
@@ -20,6 +20,72 @@
 
 import CoreImage
 
+// An alternative version of the scatter filter using a pseudo random number generator
+// inside a warp kernel.
+class ScatterWarp: CIFilter
+{
+    var inputImage: CIImage?
+    var inputScatterRadius: CGFloat = 25
+    
+    override var attributes: [String : AnyObject]
+    {
+        return [
+            kCIAttributeFilterDisplayName: "Scatter (Warp Kernel)",
+            
+            "inputImage": [kCIAttributeIdentity: 0,
+                kCIAttributeClass: "CIImage",
+                kCIAttributeDisplayName: "Image",
+                kCIAttributeType: kCIAttributeTypeImage],
+            
+            "inputScatterRadius": [kCIAttributeIdentity: 0,
+                kCIAttributeClass: "NSNumber",
+                kCIAttributeDefault: 25,
+                kCIAttributeDisplayName: "Scatter Radius",
+                kCIAttributeMin: 1,
+                kCIAttributeSliderMin: 1,
+                kCIAttributeSliderMax: 150,
+                kCIAttributeType: kCIAttributeTypeScalar],
+        ]
+    }
+    
+    let kernel = CIWarpKernel(string:
+        // based on https://www.shadertoy.com/view/ltB3zD - the additional seed
+        // calculation prevents repetition when using destCoord() as the seed.
+        "float noise(vec2 co)" +
+            "{ " +
+            "    vec2 seed = vec2(sin(co.x), cos(co.y)); " +
+            "    return fract(sin(dot(seed ,vec2(12.9898,78.233))) * 43758.5453); " +
+            "} " +
+            
+            "kernel vec2 scatter(float radius)" +
+            "{" +
+            "   float offsetX = radius * (-1.0 + noise(destCoord()) * 2.0); " +
+            "   float offsetY = radius * (-1.0 + noise(destCoord().yx) * 2.0); " +
+            "   return vec2(destCoord().x + offsetX, destCoord().y + offsetY); " +
+        "}"
+    )
+    
+    override var outputImage: CIImage?
+    {
+        guard let kernel = kernel, inputImage = inputImage else
+        {
+            return nil
+        }
+        
+        return  kernel.applyWithExtent(
+            inputImage.extent,
+            roiCallback:
+            {
+                (index, rect) in
+                return rect
+            },
+            inputImage: inputImage,
+            arguments: [inputScatterRadius])
+    }
+}
+
+// Pixel scattering filter using CIRandomGenerator as its source. The output of the
+// random generator can be blurred allowing for a smoothness attribute.
 class Scatter: CIFilter
 {
     var inputImage: CIImage?
